f11fc6493a529b3face06b01bafe9d376b58ea99
woosuk Kim <woosukkim@woosukui-MacBookPro.local>
2019-11-19 00:11:24 +0900

    first 1.10

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <h1>hi!</h1>
+        <ul>
+        {list.map((el) => {
+          return (
+            <li key={el.objectID}>
+              <div>
+                <span>
+                  <a href={el.url}>{el.title}</a>
+                </span>
+                <span>{el.author}</span>
+                <span>{el.num_comments}</span>
+                <span>{el.points}</span>
+              </div>
+            </li>
+          );
+        })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
