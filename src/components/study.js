20854f6ebb01f89db4d823a9de338cb731a41e5a
woosuk Kim <woosukkim@woosukui-MacBookPro.local>
2019-11-28 23:02:18 +0900

    react study

---
+++ src/components/study.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+class Gugudan extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            first: Math.ceil(Math.random() * 9),
+            second: Math.ceil(Math.random() * 9),
+            value: '',
+            result: ''
+        }
+    }
+    onSubmit = e => {
+        e.preventDefault();
+        if(parseInt(this.state.value) === this.state.first * this.state.second) {
+            this.setState({
+                result: '정답',
+                first: Math.ceil(Math.random() * 9),
+                second: Math.ceil(Math.random() * 9),
+                value: ''
+            })
+        }else {
+            this.setState({
+                result: '땡',
+                value: ''
+            })
+        }
+    }
+    onChange = e => this.setState({value: e.target.value})
+
+    render() {
+        return (
+            <div>
+                <p>{this.state.first}곱하기{this.state.second}는?</p>
+                <form onSubmit={this.onSubmit}>
+                    <input type="text" value={this.state.value} onChange={this.onChange}/>
+                    <button type="submit">제출</button>
+                </form>
+                <p>{this.state.result}</p>
+            </div>
+        )
+    }
+}
+
+export default Gugudan;
